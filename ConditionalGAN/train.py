e7337d254c8be3e41f5f0e7357827ba5c1e2c663
AnujCodeZ <anujrana.sid55@gmail.com>
2021-03-22 21:34:58 +0530

    Conditional GAN

---
+++ ConditionalGAN/train.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from model import Critic, Generator, initialize_weights
+from utils import gradient_penalty
+
+# Hyperparameters
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+lr = 1e-4
+batch_size = 64
+image_size = 64
+channels_img = 1
+num_classes = 10
+gen_embed = 100
+z_dim = 100
+num_epochs = 5
+features_d = 64
+features_g = 64
+critic_n = 5
+lambda_gp = 10
+
+transform = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        [0.5 for _ in range(channels_img)], 
+        [0.5 for _ in range(channels_img)], 
+    )
+])
+
+
+critic = Critic(channels_img, features_d, num_classes, image_size).to(device)
+gen = Generator(z_dim, channels_img, features_g, num_classes, image_size, gen_embed)
+initialize_weights(critic)
+initialize_weights(gen)
+fixed_noise = torch.randn(32, z_dim, 1, 1).to(device)
+
+dataset = datasets.ImageFolder(root='../data/celeb_dataset', transform=transform)
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+opt_critic = optim.Adam(critic.parameters(), lr=lr, betas=(0.0, 0.9))
+opt_gen = optim.Adam(gen.parameters(), lr=lr, betas=(0.0, 0.9))
+
+# TensorBoard
+writer_fake = SummaryWriter(f'runs/GAN_MNIST/fake')
+writer_real = SummaryWriter(f'runs/GAN_MNIST/real')
+step = 0
+
+# Training
+for epoch in range(num_epochs):
+    for batch_idx, (real, labels) in enumerate(loader):
+        real = real.to(device)
+        labels = labels.to(device)
+        
+        # For Critic
+        for _ in range(critic_n):
+            noise = torch.randn((batch_size, z_dim, 1, 1)).to(device)
+            fake = gen(noise, labels)
+            critic_real = critic(real, labels)
+            critic_fake = critic(fake, labels)
+            gp = gradient_penalty(critic, real, labels, fake, device)
+            loss_critic = -(torch.mean(critic_real) - torch.mean(critic_fake)
+                            + lambda_gp * gp)
+            
+            critic.zero_grad()
+            loss_critic.backward(retain_graph=True)
+            opt_critic.step()
+        
+        # For Generator
+        output = critic(fake, labels).view(-1)
+        loss_gen = -torch.mean(output)
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+        
+        if batch_idx == 0:
+            print(
+                f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
+                      Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
+            )
+
+            with torch.no_grad():
+                fake = gen(noise, labels)
+                img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+                img_grid_real = torchvision.utils.make_grid(real[:32], normalize=True)
+
+                writer_fake.add_image(
+                    "Fake Images", img_grid_fake, global_step=step
+                )
+                writer_real.add_image(
+                    "Real Images", img_grid_real, global_step=step
+                )
+                step += 1
---
+++ ConditionalGAN/utils.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+def gradient_penalty(critic, real, labels, fake, device='cpu'):
+    batch_size, C, H, W = real.shape
+    epsilon = torch.rand((batch_size, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated = real * epsilon + fake * (1 - epsilon)
+    
+    mixed_scores = critic(interpolated, labels)
+    
+    gradient = torch.autograd.grad(
+        inputs=interpolated,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    
+    gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    
+    return torch.mean((gradient_norm - 1) ** 2)
+    
